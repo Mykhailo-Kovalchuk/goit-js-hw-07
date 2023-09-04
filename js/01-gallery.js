685017efe65ca3dd247d7f194f79bcffa1a86061
Mykhailo-Kovalchuk <contact.kovalchuk@gmail.com>
2023-09-04 16:05:00 +0200

    goit-js-hw-07-commit 1

---
+++ js/01-gallery.js
@@ -0,0 +1,13 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galaryPhotos = document.querySelector(".gallery");
+console.log(galaryPhotos);
+
+
+const cloneGalleryItems = galleryItems.cloneNode(true);
+
+// galaryPhotos.classList.add('galleryItems');
+
